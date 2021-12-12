27cb9287bbf461ea84022a22ba5226d2b625eae8
davidyoon <qqpo12@naver.com>
2021-12-12 16:28:30 +0900

    set UILayout for water drink alert app and apply NotificationCenter for local notification

---
+++ Drink/AddAlertViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddAlertViewController.swift
+//  Drink
+//
+//  Created by David Yoon on 2021/12/12.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController {
+
+    var pickedDate: ((_ date: Date) -> Void)?
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func dismissButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Drink/AlertListCell.swift
@@ -0,0 +1,39 @@
+//
+//  AlertListCell.swift
+//  Drink
+//
+//  Created by David Yoon on 2021/12/12.
+//
+
+import UIKit
+
+class AlertListCell: UITableViewCell {
+
+    @IBOutlet weak var meridiemLable: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var alertSwitch: UISwitch!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func alertSwitchValueChanged(_ sender: UISwitch) {
+        guard let data = UserDefaults.standard.value(forKey: "alerts") as? Data,
+              var alerts = try? PropertyListDecoder().decode([Alert].self, from: data) else {
+            return
+        }
+        
+        alerts[sender.tag].isOn = sender.isOn
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(alerts), forKey: "alerts")
+    }
+    
+}
